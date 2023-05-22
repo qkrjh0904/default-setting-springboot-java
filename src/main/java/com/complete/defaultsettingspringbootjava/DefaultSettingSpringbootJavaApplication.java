ca62285278eeabb162aad83afecdc53d98d2cb20
jeongho <qkrjh0904@gmail.com>
2023-05-22 21:39:22 +0900

    init

---
+++ src/main/java/com/complete/defaultsettingspringbootjava/DefaultSettingSpringbootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.complete.defaultsettingspringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultSettingSpringbootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultSettingSpringbootJavaApplication.class, args);
+	}
+
+}
